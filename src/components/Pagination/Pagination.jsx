aa6eb0d6a3d2a8473ddcda39dea64b42da15c815
mirzamujezinovic <mikigithub@gmail.com>
2024-07-15 00:46:15 +0200

    Pravljenje paginacije

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,47 @@
+import "./Pagination.css";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
+export function Pagination({ brojStranica, setPage, page }) {
+  const niz = [];
+  for (let i = 1; i <= brojStranica; i++) {
+    niz.push(i);
+  }
+
+  return (
+    <div className="pagination">
+      <button
+        className="pagination-btn"
+        onClick={() => {
+          setPage(page - 1);
+        }}
+        disabled={page === 1}
+      >
+        <MdKeyboardArrowLeft className="pagination-strelica" />
+      </button>
+      {niz.map((value, index) => {
+        const isActive = page === value;
+        return (
+          <button
+            onClick={() => {
+              setPage(value);
+            }}
+            key={index}
+            style={{ fontSize: "1.25em" }}
+            className={`pagination-btn ${isActive ? "active" : ""}`}
+          >
+            {value}
+          </button>
+        );
+      })}
+      <button
+        className="pagination-btn"
+        onClick={() => {
+          setPage(page + 1);
+        }}
+        disabled={page === brojStranica}
+      >
+        <MdKeyboardArrowRight className="pagination-strelica" />
+      </button>
+    </div>
+  );
+}
