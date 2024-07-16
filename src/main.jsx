bdf5948379ddc80b754b135a3fd3f95431987d5a
mirzamujezinovic <mikigithub@gmail.com>
2024-07-17 00:39:00 +0200

    vrepovano sa app contextom

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
+import { ContextProvider } from './context/AppContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
     <BrowserRouter>
     <MantineProvider>
+      <ContextProvider>
         <App />
+      </ContextProvider>
     </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>,
