4742baa2bf95b98d8ac8eb7ccd6199108ebdfdbb
mirzamujezinovic <mikigithub@gmail.com>
2024-07-17 00:40:47 +0200

    page sa novom karticom

--- src/pages/Outlet/Outlet.jsx
+++ src/pages/Outlet/Outlet.jsx
@@ -1,6 +1,43 @@
+import ProductCardOnSale from "../../components/ProductCardOnSale/ProductOnSale";
+import productsOnSale from "../../common/productsOnSale.json";
+import { Pagination } from "../../components/Pagination/Pagination";
+import { useState, useEffect } from "react";
+import "./Outlet.css";
+
+
 export default function Outlet() {
-    return (
+    const [page, setPage] = useState(1)
+  const brojProducta = productsOnSale.length
+  const brojProductaPoStranici = 6
+  const brojStranica = Math.ceil(brojProducta / brojProductaPoStranici)
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [page]);
+  return (
     <>
+  <div className="producti">
+    <div className="onsale-wrapper-product">
+      {productsOnSale.map((product) => {
+        return (
+          <ProductCardOnSale
+            slika={product.image_url}
+            description={product.short_description}
+            cena={product.current_price}
+            onClick={()=>addToCart(product)}
+          />
+        );
+      }).slice(
+        brojProductaPoStranici * (page - 1),
+        brojProductaPoStranici * page
+      )}
+    </div>
+    <Pagination brojStranica={brojStranica} setPage={setPage} page={page} />
+  </div>
+  ;
+
     </>
     );
   }
