c5ea7a45a55b314ecaa01b246cfe11b5315a7a63
mirzamujezinovic <mikigithub@gmail.com>
2024-07-18 13:23:28 +0200

    pravljenje korpe nij e dovrsen do kraja

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,36 @@
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
+import "./Cart.css";
 
 export default function Cart() {
-    const {productsInCart} = useContext(AppContext)
-    return ( 
-    <> 
-    </> 
-    );
-  }
+  const { productsInCart, removeFromCart } = useContext(AppContext);
+
+  return (
+    <div className="cart-container">
+      {productsInCart.length === 0 ? (
+        <div className="cart-empty">
+          <img src="https://www.adasglobal.com/img/empty-cart.png" alt="prazna" />
+        </div>
+      ) : (
+        productsInCart.map((item) => (
+          <div key={item.id} className="cart-item">
+            <img src={item.image_url} alt={item.title} className="cart-item-image" />
+            <div className="cart-item-info">
+              <h3 className="cart-item-name">{item.title}</h3>
+              {item.discountedPrice && (
+                <div className="cart-item-prices">
+                  <p className="cart-item-discount">{item.original_price} RSD</p>
+                  <p className="cart-item-price">{item.current_price} RSD</p>
+                </div>
+              )}
+              {!item.discountedPrice && (
+                <p className="cart-item-price">{item.current_price} RSD</p>
+              )}
+              <button onClick={() => removeFromCart(item)} className="cart-item-remove">Remove</button>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
