e49acf2549b74f58be26d2c9818a7f68cdf63604
mirzamujezinovic <mikigithub@gmail.com>
2024-07-23 15:10:45 +0200

    Updateovan footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,21 @@
 import "./Footer.css";
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
 
 const Footer = () => {
   return (
     <>
       <footer className="footer">
         <h1 style={{ fontSize: "30px", marginLeft: "50px" }}>ShoppingCart ©</h1>
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      <p>Novi Pazar 36300</p>
+      <p>Contact us at: mirzamujezinovic212@gmail.com</p>
+      <p>Phone number: +3810666310631</p>
+      <div className="social-icons">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+      </div>
       </footer>
     </>
   );
